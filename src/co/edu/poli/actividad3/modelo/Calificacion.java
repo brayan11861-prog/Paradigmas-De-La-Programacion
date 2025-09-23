d8577ea8d547a81b7d15c23852e8ba9e0b01adf6
brayan11861-prog <brayan11861@gmail.com>
2025-09-23 13:43:16 -0500

    Update Calificacion.java

--- src/co/edu/poli/actividad3/modelo/Calificacion.java
+++ src/co/edu/poli/actividad3/modelo/Calificacion.java
@@ -12,47 +12,36 @@ public class Calificacion {
 		this.nombre = nombre;
 		this.descripcion = descripcion;
 	}
-
 	public Calificacion(String serial, char categoria) {
 		this(serial, categoria, "Sin nombre", "Sin descripcion");
 	}
-
 	public String getSerial() {
 		return serial;
 	}
-
 	public void setSerial(String serial) {
 		this.serial = serial;
 	}
-
 	public char getCategoria() {
 		return categoria;
 	}
-
 	public void setCategoria(char categoria) {
 		this.categoria = categoria;
 	}
-
 	public String getNombre() {
 		return nombre;
 	}
-
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
 	public String getDescripcion() {
 		return descripcion;
 	}
-
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
-
 	public void evaluar() {
 		System.out.println("La obra " + nombre + " recibio una calificacion categoria " + categoria);
 	}
-
 	@Override
 	public String toString() {
 		return "CALIFICACION: " + "(serial='" + serial + "')|" + " (categoria='" + categoria + "')|" + " (nombre='"
