adceeb67b6cac998468efc0da72afb706a416d9c
brayan11861-prog <brayan11861@gmail.com>
2025-09-25 09:50:42 -0500

    Update Actividad.java

--- src/co/edu/poli/actividad3/modelo/Actividad.java
+++ src/co/edu/poli/actividad3/modelo/Actividad.java
@@ -1,42 +1,65 @@
 package co.edu.poli.actividad3.modelo;
+
+/**
+ * Clase abstracta que representa una actividad genérica. Sirve como superclase
+ * para eventos, exposiciones y visitas. Contiene serial, nombre, fecha de
+ * inicio y fecha de fin.
+ * 
+ * @author Brayan Niño
+ * @version 1.0
+ */
 public abstract class Actividad {
 	private String serial;
 	private String nombre;
 	private String fechainicio;
 	private String fechafin;
 
-	// Constructor completo
 	public Actividad(String serial, String nombre, String fechainicio, String fechafin) {
-	this.serial = serial;
-	this.nombre = nombre;
-	this.fechainicio = fechainicio;
-	this.fechafin = fechafin;
+		this.serial = serial;
+		this.nombre = nombre;
+		this.fechainicio = fechainicio;
+		this.fechafin = fechafin;
 	}
 
-	// Constructor sobrecargado
 	public Actividad(String serial, String nombre) {
-	this(serial, nombre, "Sin inicio", "Sin fin");
+		this(serial, nombre, "Sin inicio", "Sin fin");
 	}
 
-	// Getters y setters
-	public String getSerial() { return serial; }
-	public void setSerial(String serial) { this.serial = serial; }
+	public String getSerial() {
+		return serial;
+	}
 
-	public String getNombre() { return nombre; }
-	public void setNombre(String nombre) { this.nombre = nombre; }
+	public void setSerial(String serial) {
+		this.serial = serial;
+	}
 
-	public String getFechainicio() { return fechainicio; }
-	public void setFechainicio(String fechainicio) { this.fechainicio = fechainicio; }
+	public String getNombre() {
+		return nombre;
+	}
 
-	public String getFechafin() { return fechafin; }
-	public void setFechafin(String fechafin) { this.fechafin = fechafin; }
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getFechainicio() {
+		return fechainicio;
+	}
+
+	public void setFechainicio(String fechainicio) {
+		this.fechainicio = fechainicio;
+	}
+
+	public String getFechafin() {
+		return fechafin;
+	}
+
+	public void setFechafin(String fechafin) {
+		this.fechafin = fechafin;
+	}
 
 	@Override
 	public String toString() {
-	return "ACTIVIDAD: " +
-	"(serial='" + serial + ")|" +
-	"(nombre='" + nombre + ")|" +
-	"(fechainicio='" + fechainicio + ")|" +
-	"(fechafin='" + fechafin + ")|" ;
+		return "ACTIVIDAD: " + "(serial='" + serial + ")|" + "(nombre='" + nombre + ")|" + "(fechainicio='"
+				+ fechainicio + ")|" + "(fechafin='" + fechafin + ")|";
 	}
-	}
+}
