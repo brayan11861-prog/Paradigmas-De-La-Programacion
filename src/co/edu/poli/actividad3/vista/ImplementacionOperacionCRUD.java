86399ac9abc658b7c57e4a4af2a2ef4097ae225e
brayan11861-prog <brayan11861@gmail.com>
2025-10-23 13:03:40 -0500

    Update ImplementacionOperacionCRUD.java

--- src/co/edu/poli/actividad3/vista/ImplementacionOperacionCRUD.java
+++ src/co/edu/poli/actividad3/vista/ImplementacionOperacionCRUD.java
@@ -1,132 +1,71 @@
-package co.edu.poli.actividad3.servicios;
+package co.edu.poli.actividad3.vista;
 
-import co.edu.poli.actividad3.modelo.Actividad; // o la superclase que tengas
-import java.util.Arrays;
+import co.edu.poli.actividad3.modelo.Actividad;
 
-/**
- * Implementación genérica de operaciones CRUD usando un arreglo estático de tipo superclase
- * 
- * @param <T> Tipo de objeto que extiende de la superclase
- */
-public class ImplementacionOperacionCRUD<T> implements OperacionCRUD<T> {
+public class ImplementacionOperacionCRUD implements OperacionCRUD {
 
-    private static final int INCREMENTO = 5;
-    private T[] arreglo;
-    private int size; // cantidad de elementos (no la capacidad)
+    private Actividad[] lista = new Actividad[5];
 
-    /**
-     * Constructor que inicializa el arreglo con tamaño inicial
-     */
-    @SuppressWarnings("unchecked")
-    public ImplementacionOperacionCRUD() {
-        // Crear arreglo de tamaño inicial (INCREMENTO)
-        arreglo = (T[]) new Object[INCREMENTO];
-        size = 0;
-    }
-
-    /**
-     * Inserta un objeto en el primer espacio null del arreglo. Si está lleno,
-     * aumenta el tamaño del arreglo.
-     */
     @Override
-    public void create(T objeto) {
-        // Buscar primer null
-        for (int i = 0; i < arreglo.length; i++) {
-            if (arreglo[i] == null) {
-                arreglo[i] = objeto;
-                size++;
-                return;
+    public boolean create(Actividad a) {
+        for (int i = 0; i < lista.length; i++) {
+            if (lista[i] == null) {
+                lista[i] = a;
+                return true;
             }
         }
-        // Si no hay espacio, expandir arreglo y agregar al final
-        expandirArreglo();
-        arreglo[size++] = objeto;
+
+        // Aumentar tamaño cuando esté lleno
+        Actividad[] nuevaLista = new Actividad[lista.length * 2];
+        System.arraycopy(lista, 0, nuevaLista, 0, lista.length);
+        nuevaLista[lista.length] = a;
+        lista = nuevaLista;
+        return true;
     }
 
-    /**
-     * Busca un objeto por ID usando reflexión
-     */
     @Override
-    public T read(String id) {
-        for (int i = 0; i < arreglo.length; i++) {
-            if (arreglo[i] != null) {
-                try {
-                    String objId = arreglo[i].getClass().getMethod("getId").invoke(arreglo[i]).toString();
-                    if (objId.equals(id)) {
-                        return arreglo[i];
-                    }
-                } catch (Exception e) {
-                    // Ignorar error de reflexión
-                }
+    public Actividad read(String serial) {
+        for (Actividad a : lista) {
+            if (a != null && a.getSerial().equals(serial)) {
+                return a;
             }
         }
         return null;
     }
 
-    /**
-     * Actualiza un objeto por ID usando reflexión
-     */
     @Override
-    public boolean update(String id, T objeto) {
-        for (int i = 0; i < arreglo.length; i++) {
-            if (arreglo[i] != null) {
-                try {
-                    String objId = arreglo[i].getClass().getMethod("getId").invoke(arreglo[i]).toString();
-                    if (objId.equals(id)) {
-                        arreglo[i] = objeto;
-                        return true;
-                    }
-                } catch (Exception e) {
-                    // Ignorar error
-                }
+    public boolean update(String serial, Actividad nueva) {
+        for (int i = 0; i < lista.length; i++) {
+            if (lista[i] != null && lista[i].getSerial().equals(serial)) {
+                lista[i] = nueva;
+                return true;
             }
         }
         return false;
     }
 
-    /**
-     * Elimina un objeto por ID, dejando espacio null para futuras inserciones
-     */
     @Override
-    public boolean delete(String id) {
-        for (int i = 0; i < arreglo.length; i++) {
-            if (arreglo[i] != null) {
-                try {
-                    String objId = arreglo[i].getClass().getMethod("getId").invoke(arreglo[i]).toString();
-                    if (objId.equals(id)) {
-                        arreglo[i] = null;
-                        size--;
-                        return true;
-                    }
-                } catch (Exception e) {
-                    // Ignorar error
-                }
+    public boolean delete(String serial) {
+        for (int i = 0; i < lista.length; i++) {
+            if (lista[i] != null && lista[i].getSerial().equals(serial)) {
+                lista[i] = null;
+                return true;
             }
         }
         return false;
     }
 
-    /**
-     * Retorna un arreglo filtrado con los objetos actuales (sin los null)
-     */
     @Override
-    public T[] list() {
-        @SuppressWarnings("unchecked")
-        T[] resultado = (T[]) new Object[size];
-        int idx = 0;
-        for (int i = 0; i < arreglo.length; i++) {
-            if (arreglo[i] != null) {
-                resultado[idx++] = arreglo[i];
+    public void listar() {
+        boolean vacio = true;
+        for (Actividad a : lista) {
+            if (a != null) {
+                System.out.println(a);
+                vacio = false;
             }
         }
-        return resultado;
-    }
-
-    /**
-     * Aumenta la capacidad del arreglo en INCREMENTO
-     */
-    @SuppressWarnings("unchecked")
-    private void expandirArreglo() {
-        arreglo = Arrays.copyOf(arreglo, arreglo.length + INCREMENTO);
+        if (vacio) {
+            System.out.println("No hay actividades registradas.");
+        }
     }
 }
